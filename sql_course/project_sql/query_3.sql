65d7538fa148d503cefbf53f485707523ef9cb98
Imtiaz <imtist21@gmail.com>
2025-07-10 17:50:12 +0100

    Readme

--- sql_course/project_sql/query_3.sql
+++ sql_course/project_sql/query_3.sql
@@ -1,6 +1,3 @@
-/* 
-What are the most in demand skills for data analysts in London
-*/;
 
 SELECT 
     skills,
@@ -10,8 +7,9 @@ INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
+    AND job_location = 'London, UK'
 GROUP BY
     skills
 ORDER BY
     demand_count DESC
-LIMIT 5
+LIMIT 10
--- sql_course/project_sql/query_4.sql
+++ sql_course/project_sql/query_4.sql
@@ -11,19 +11,10 @@ INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
+    AND job_location = 'London, UK'
 GROUP BY
     skills
 ORDER BY
     avg_salary DESC
-LIMIT 25;
+LIMIT 10;
 
-/*
-✅ AI, Machine Learning & Deep Learning tools drive top salaries
-Skills like DataRobot, PyTorch, Hugging Face, TensorFlow, and others command high pay as businesses race to build, deploy, and operationalize AI products. Expertise in these areas is scarce, especially in production-level AI systems.
-
-✅ Cloud Infrastructure, DevOps & Data Engineering are critical and lucrative
-High-paying skills such as Terraform, Kafka, Airflow, VMware, Ansible, and Cassandra reflect the need for robust, scalable, real-time data pipelines and cloud environments that support modern AI, analytics, and software systems.
-
-✅ Niche, specialized, or emerging tech offers a scarcity premium
-Technologies like Solidity (blockchain), Golang (cloud-native apps), and Couchbase (NoSQL) are in demand but with fewer experts available, leading to premium pay. Even older legacy skills like Perl or SVN can command high salaries for rare maintenance roles.
-*/
